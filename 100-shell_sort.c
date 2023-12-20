cd2c5faaf820d980e9844d71d08c12a9b64dccd0
oluwakuty <oyindamoanu@gmail.com>
2023-12-19 19:24:30 -0800

    sorting algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,16 +2,16 @@
 
 /**
  * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * @b: The first integer to swap.
+ * @c: The second integer to swap.
  */
-void swap_ints(int *a, int *b)
+void swap_ints(int *b, int *c)
 {
-	int tmp;
+	int temp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	temp = *b;
+	*b = *c;
+	*c = temp;
 }
 
 /**
@@ -19,28 +19,27 @@ void swap_ints(int *a, int *b)
  *              order using the shell sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
- *
  * Description: Uses the Knuth interval sequence.
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	size_t pap, k, l;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (gap = 1; gap < (size / 3);)
-		gap = gap * 3 + 1;
+	for (pap = 1; pap < (size / 3);)
+		pap = pap * 3 + 1;
 
-	for (; gap >= 1; gap /= 3)
+	for (; pap >= 1; pap /= 3)
 	{
-		for (i = gap; i < size; i++)
+		for (k = pap; k < size; k++)
 		{
-			j = i;
-			while (j >= gap && array[j - gap] > array[j])
+			l = k;
+			while (l >= pap && array[j - pap] > array[j])
 			{
-				swap_ints(array + j, array + (j - gap));
-				j -= gap;
+				swap_ints(array + l, array + (l - pap));
+				l -= pap;
 			}
 		}
 		print_array(array, size);
